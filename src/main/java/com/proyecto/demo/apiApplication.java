67f20b2399153f999fc4de1c9cd9dc74f0cc2b08
Martin Salinas Scussolin <martin.salinas.scussolin@gmail.com>
2020-07-20 00:36:48 -0400

    un xd mas

--- src/main/java/com/proyecto/demo/apiApplication.java
+++ src/main/java/com/proyecto/demo/apiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class apiApplication {
 
 	public static void main(String[] args) {
+		System.out.println("lamao soy una app y estoy corriendo");
 		SpringApplication.run(apiApplication.class, args);
 	}
 
