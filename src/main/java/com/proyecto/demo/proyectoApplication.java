de94c089943c591114fd9324bae597a8a157530e
Martin Salinas Scussolin <martin.salinas.scussolin@gmail.com>
2020-07-20 00:43:46 -0400

    pequeño fix nombre de main

--- src/main/java/com/proyecto/demo/proyectoApplication.java
+++ src/main/java/com/proyecto/demo/proyectoApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class apiApplication {
+public class proyectoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("lamao soy una app y estoy corriendo");
-		SpringApplication.run(apiApplication.class, args);
+		SpringApplication.run(proyectoApplication.class, args);
 	}
 
 }
